046bf709eed590de8312ffbe3a58269d6f138127
donald <donald.rupin@pm.me>
2022-01-26 21:28:10 +1100

    Reduced size of io_uring even further to test with actions

--- includes/zab/event_loop.hpp
+++ includes/zab/event_loop.hpp
@@ -70,7 +70,7 @@ namespace zab {
 
             class descriptor;
 
-            static constexpr auto kQueueSize = 4096;
+            static constexpr auto kQueueSize = 512;
 
             /**
              * @brief      Constructs a new instance that will register to this engine.
